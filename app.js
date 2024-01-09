20eccc0b44ce3902bebb501b39d3f93abbbca8e0
Téglás Ádám <ad.teglas@gmail.com>
2024-01-09 17:24:49 +0100

    Done

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
 const minus = document.querySelector(".negative");
 const plus = document.querySelector(".positive");
-const numberText = document.querySelector(".number");
+const numberText = document.querySelector(".numberText");
 let number = 0;
 
 
 minus.addEventListener("click", function() {
     number--;
     numberText.innerHTML = number;
+    if (number === 0) {
+        numberText.style.color = "black";
+    }
+    else if (number <= 0) {
+        numberText.style.color = "red";
+    }
+    else if (number >= 0) {
+        numberText.style.color = "green";
+    }
+    console.log(number);
 })
 
 plus.addEventListener("click", function() {
     number++;
     numberText.innerHTML = number;
+    if (number === 0) {
+        numberText.style.color = "black";
+    }
+    else if (number <= 0) {
+        numberText.style.color = "red";
+    }
+    else if (number >= 0) {
+        numberText.style.color = "green";
+    }
+    console.log(number);
 })
