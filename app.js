9824698daed00c3db5b7153dc4879dafa32759c9
Téglás Ádám <ad.teglas@gmail.com>
2024-01-09 17:45:39 +0100

    javitott kod FASZA

--- app.js
+++ app.js
@@ -2,34 +2,30 @@ const minus = document.querySelector(".negative");
 const plus = document.querySelector(".positive");
 const numberText = document.querySelector(".numberText");
 let number = 0;
+let x;
 
+let check = () => {
+	if (x == 0) {
+		numberText.style.color = "black";
+	} else if (x == -1) {
+		numberText.style.color = "red";
+	} else if (x == 1) {
+		numberText.style.color = "green";
+	}
+};
 
-minus.addEventListener("click", function() {
-    number--;
-    numberText.innerHTML = number;
-    if (number === 0) {
-        numberText.style.color = "black";
-    }
-    else if (number <= 0) {
-        numberText.style.color = "red";
-    }
-    else if (number >= 0) {
-        numberText.style.color = "green";
-    }
-    console.log(number);
-})
+minus.addEventListener("click", function () {
+	number--;
+	x = Math.sign(number);
+	numberText.innerHTML = number;
+	check();
+	return x;
+});
 
-plus.addEventListener("click", function() {
-    number++;
-    numberText.innerHTML = number;
-    if (number === 0) {
-        numberText.style.color = "black";
-    }
-    else if (number <= 0) {
-        numberText.style.color = "red";
-    }
-    else if (number >= 0) {
-        numberText.style.color = "green";
-    }
-    console.log(number);
-})
+plus.addEventListener("click", function () {
+	number++;
+	x = Math.sign(number);
+	numberText.innerHTML = number;
+	check();
+	return x;
+});
